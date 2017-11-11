0cfc9f9e2e0443a3d10ac64cb25c4d4a6397a5e7
Mario  Gamboa-Diaz <mariodiaz5531@gmail.com>
2017-11-11 23:08:28 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,11 +3,8 @@ function happyHolidays(string) {
   return "Happy holidays!"
 }
 function happyHolidaysTo(name){
-  var name = "Rene"
   return (`Happy holidays, ${name}!`)
 }
 function happyHolidayTo(holiday,name){
-  var name = "Rene"
-  var holiday = "Christmas"
-  return (`Happy ${holiday}, ${}`)
+  return (`Happy ${holiday}, ${name}`)
 }
