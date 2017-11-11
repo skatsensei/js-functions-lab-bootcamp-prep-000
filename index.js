21edf8df635cba7f062cd1800207c5fb3ecb55f0
Mario  Gamboa-Diaz <mariodiaz5531@gmail.com>
2017-11-11 23:14:51 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -8,3 +8,6 @@ function happyHolidaysTo(name){
 function happyHolidayTo(holiday,name){
   return (`Happy ${holiday}, ${name}!`)
 }
+funciton holidayCountdown(days,holiday){
+  return (`I'ts ${days} days until ${holiday}!`
+  
