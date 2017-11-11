80d2caf5384a13e63dc419a7113b36d5b55683a7
Mario  Gamboa-Diaz <mariodiaz5531@gmail.com>
2017-11-11 22:18:10 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // write your code below!
+function happyHolidays(string) {
+  return "Happy holidays!"
+}
+function happyHolidaysTo(name){
+  
+  return `Happy holidays, ${name}!`
+}
+function happyHolidayTo(holiday,name){
+  var
+}
